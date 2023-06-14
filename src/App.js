9292cc29d84974944f33d0a1904c530369a3a726
Sadaf khan <GFG19190@gfg.geeksforgeeks.org>
2023-06-14 18:40:11 +0530

    Added code for debounce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleInputChange=(e)=> {
+    fetch('https://dummyjson.com/products/1')
+    .then((data) => data.json())
+    .then((data) => console.log(data))
+  }
+  // const debounce = (callback, delay) => {
+  //   let timerId = "";
+  //   return (...args) =>{
+  //     clearTimeout(timerId);
+  //     timerId = setTimeout (() => {
+  //       callback(args);
+  //     },delay)
+  //   }
+  // }
+const debounce=(callback, delay) =>{
+  let timerId = "";
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(args)
+    },delay)
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='Enter search item' onChange={debounce(handleInputChange,1000)}/>
     </div>
   );
 }
